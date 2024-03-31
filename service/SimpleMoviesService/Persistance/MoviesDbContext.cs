fe81c535393d9bdfa44f0af37439521943d28313
Will <willstrach@gmail.com>
2024-03-31 19:34:22 +0100

    Specified column type for decimals

--- service/SimpleMoviesService/Persistance/MoviesDbContext.cs
+++ service/SimpleMoviesService/Persistance/MoviesDbContext.cs
@@ -7,4 +7,16 @@ public class MoviesDbContext(DbContextOptions<MoviesDbContext> options) : DbCont
 {
     public DbSet<Movie> Movies { get; set; }
     public DbSet<Genre> Genres { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Movie>()
+            .Property(movie => movie.Popularity)
+            .HasColumnType("decimal(8, 4)");
+
+
+        builder.Entity<Movie>()
+            .Property(movie => movie.VoteAverage)
+            .HasColumnType("decimal(5, 2)");
+    }
 }
